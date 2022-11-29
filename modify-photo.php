2a01b9ad0dc24a8a8ee650ab87aebfcd9df2169f
gamingamerman <gamingamerman@gmail.com>
2022-11-29 17:21:14 +0100

    First commit on branch modify-photo: Mock modify Page

---
+++ modify-photo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhotoMenu</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Modify Photo</legend>
+        <form action="">
+            <p>Author: <input type="text" name="author" placeholder="Author...">
+            </p>
+            <p>
+                <h3>Photo:</h3>
+                <br>
+                <input type="file" name="file">
+            </p>
+            <input type="submit" name="send" value="Modify">
+        </form>
+    </fieldset>
+</body>
+</html>
