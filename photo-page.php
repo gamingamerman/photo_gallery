3890c5e8e9a64a69ab7cfb50543184b354dbf991
Favio <favio2santos@gmail.com>
2022-11-30 20:09:25 +0000

    photo-user fix

--- photo-page.php
+++ photo-page.php
@@ -16,6 +16,8 @@ session_start();
         <thead>
             <th>Photo</th>
             <th>Author</th>
+            <th>Description</th>
+            <th>Options</th>
         </thead>
         <tbody>
             <?php
@@ -26,13 +28,18 @@ session_start();
             ?>
             <tr>
                 <td><img src="/img/<?=$row['name']?>" alt=""></td>
+
                 <?php
                 $sql2 = 'SELECT id, name FROM authors';
                 $result2 = $link->query($sql2);
+
                 while ($row2 = $result2->fetch_array()):
                     if ($row2['id'] == $row['author_id']):
                 ?>
+
                 <td><?=$row2['name']?></td>
+                <td><?=$row['text']?></td>
+
                 <?php
                 endif;
                 endwhile;
